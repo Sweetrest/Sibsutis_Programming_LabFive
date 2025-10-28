c6a6a2563ba1ee30fdc356d3c844ecd33a965e7d
Rostislav Tartyshnyi Igorevich IP-516 <rostislav072005@mail.ru>
2025-10-29 02:26:37 +0700

    Fix ex5-1

--- src/exercise5.c
+++ src/exercise5.c
@@ -2,7 +2,6 @@
 
 int main() {
     double a[10][3], b[3][10], c[10][10] = {0};
-
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 3; j++) {
             scanf("%lf", &a[i][j]);
@@ -22,7 +21,10 @@ int main() {
     }
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            printf("%.2f ", c[i][j]);
+            printf("%.2f", c[i][j]);
+            if (!(i == 9 && j == 9)) {
+                printf(" ");
+            }
         }
     }
     printf("\n");
